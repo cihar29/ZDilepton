ef12199fc1bcd3566250edcfa8f02fb68089f8bd
Chad Harrington <cihar29@fnal.gov>
2017-05-11 01:12:11 -0500

    added more histos

--- plugins/ZDilepton.cc
+++ plugins/ZDilepton.cc
@@ -140,7 +140,10 @@ class ZDilepton : public edm::EDAnalyzer {
     float met_shiftedpx[METUNCERT], met_shiftedpy[METUNCERT];
 
     TH1F* fullMu = new TH1F("fullMu","fullMu",100,0,100);
+    TH1F* deltaT_pT = new TH1F("deltaT_pT","deltaT_pT",100,-500,500);
+    TH1F* deltaTbar_pT = new TH1F("deltaTbar_pT","deltaTbar_pT",100,-500,500);
     TH2F* ttbar_pT = new TH2F("ttbar_pT","ttbar_pT",50,0,1000,50,0,1000);
+    TH2F* ttbar_pT2 = new TH2F("ttbar_pT2","ttbar_pT2",50,0,1000,50,0,1000);
 
     TString fileName_;
     string btag_;
@@ -347,17 +350,29 @@ void ZDilepton::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     edm::Handle< edm::View<reco::GenParticle> > genParticles;
     iEvent.getByToken(genParticleTag_, genParticles);
 
-    float t_pT=-1, tbar_pT=-1;
+    float t_pT=-1, tbar_pT=-1, t_pT2=-1, tbar_pT2=-1;
     for (int i=0, n=genParticles->size(); i<n; i++) {
       const reco::GenParticle& p = genParticles->at(i);
 
       int id = p.pdgId();
       int status = p.status();
+      int nDaught = p.numberOfDaughters();
 
       if (id==6 && 20<=status && status<30) t_pT = p.pt();
       else if (id==-6 && 20<=status && status<30) tbar_pT = p.pt();
+
+      else if (id==6 && nDaught==2) t_pT2 = p.pt();
+      else if (id==-6 && nDaught==2) tbar_pT2 = p.pt();
     }
+
     ttbar_pT->Fill(t_pT, tbar_pT);
+
+    if ( t_pT2 != -1 && tbar_pT2 != -1 ) {
+
+      ttbar_pT2->Fill(t_pT2, tbar_pT2);
+      deltaT_pT->Fill(t_pT-t_pT2);
+      deltaTbar_pT->Fill(tbar_pT-tbar_pT2);
+    }
   }
   else{
     mu = getAvgPU( run, lumi );
@@ -844,6 +859,16 @@ void ZDilepton::endJob() {
 
   if (root_file !=0) {
 
+    root_file->cd();
+
+    fullMu->Write();
+    if (isMC_) {
+      ttbar_pT->Write();
+      ttbar_pT2->Write();
+      deltaT_pT->Write();
+      deltaTbar_pT->Write();
+    }
+
     root_file->WriteObject(&totalEvts, "totalEvts");
     root_file->WriteObject(&filter_failed, "filter_failed");
     root_file->WriteObject(&dilep_cut, "dilep_cut");
@@ -851,9 +876,6 @@ void ZDilepton::endJob() {
     root_file->WriteObject(&jetpteta_cut, "jetpteta_cut");
     root_file->WriteObject(&met_cut, "met_cut");
     root_file->WriteObject(&dilepmass_cut, "dilepmass_cut");
-
-    fullMu->Write();
-    if (isMC_) ttbar_pT->Write();
 
     root_file->Write();
     delete root_file;
