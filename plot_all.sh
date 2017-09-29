5c829b748fb3f5d5a50b0186aa64742d21865d21
Bahareh <broozbah@buffalo.edu>
2017-09-29 11:41:13 -0500

    q2

--- plot_all.sh
+++ plot_all.sh
@@ -12,7 +12,7 @@ if [ $# -eq 2 ] ; then
 
   channel=${args[0]}
   cut=${args[1]}
-  dir="/uscms_data/d3/cihar29/Analysis/CMSSW_8_0_26_patch2/src/analysis/ZDilepton/"
+  dir="root_trees/SIGNAL/"
 
   if [[ $channel = "mm" ]] ; then
     dir="${dir}mm/"
@@ -54,13 +54,15 @@ if [ $# -eq 2 ] ; then
     rebin="2"
     if [[ "${hists[i]}" == *"met"* ]] ; then
       rebin="0 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 2500 5000"
+    elif [[ "${hists[i]}" == "sT" || "${hists[i]}" == "jethT" || "${hists[i]}" == "masslljjm" ]] ; then
+      rebin="0 50 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000 1050 1100 1150 1200 1250 1300 1350 1400 1450 1500 1600 1800 2000 2500 3000 5000"
     elif [[ "${hists[i]}" == *"Good"* || "${hists[i]}" == "nbtag" ]] ; then
       rebin="1"
     fi
 
     lines=( "dataFileName   ${dir}${dataset}_${channel}.root"
             "dataName       Data"
-            "plotData       true"
+            "plotData       false"
             "mcFileNames    ${dir}DYhigh_${channel}.root ${dir}DYlow_${channel}.root ${dir}STschannel_${channel}.root ${dir}STtWchannel_${channel}.root ${dir}STtchannel_${channel}.root ${dir}SaTtWchannel_${channel}.root ${dir}SaTtchannel_${channel}.root ${dir}TTbar_${channel}.root ${dir}WJets_${channel}.root ${dir}WW_${channel}.root ${dir}WZ_${channel}.root ${dir}ZZ_${channel}.root"
             "sigFileNames   ${dir}zprime_M-3000_W-300_${channel}.root ${dir}gluon_M-3000_${channel}.root"
             "sigScales      1 1"
@@ -69,14 +71,14 @@ if [ $# -eq 2 ] ; then
             "logx           false"
             "logy           ${logy}"
             "subplot        ${subplot}"
-            "outName        ${cut}/${hists[i]}"
+            "outName        Sep25/SetDRCut/${channel}/${cut}/${hists[i]}"
             "hname          ${cut}_${hists[i]}"
             "xmin           ${xmins[i]}"
             "xmax           ${xmaxs[i]}"
             "ymin           0"
             "subymin        ${subymin}"
             "subymax        2"
-            "systematics    topPt_weight jec jer btagSF mistagSF pileup pdf q2"
+            "systematics    topPt_weight jec jer btagSF mistagSF pileup pdf q2ttbar q2dy q2st q2signal"
             "sys_norm       lumi:0.025 sig_st:0.16 sig_db:0.15 mutrig:0.005 muid:0.01 muiso:0.01 eltrig:0.05 elid:0.01 eliso:0.01"
             "rebin          ${rebin}"
             "plotImpact     false"
